71f74d3bae4c428bc107fdf5cac8e3e35e82f882
Filippo Savi <filisavi@unimore.it>
2025-03-12 17:52:23 +0100

    sbetter structure for the test/operating models folder

---
+++ models/cc_dab/cc_dab.cpp
@@ -0,0 +1,139 @@
+
+#include "cc_dab.hpp"
+
+std::vector<float> cc_dab(const std::vector<float>&inputs, std::vector<float>&state) {
+
+    std::vector<float> outputs(2, 0);
+
+    // OUTPUT
+
+    #define in_v_in inputs[0]
+    #define in_en inputs[1]
+    #define in_i_out inputs[2]
+
+    #define out_v_out outputs[0]
+    #define out_i_in outputs[1]
+
+    #define mem_i_in_past state[0]
+    #define mem_i_pri_prev state[1]
+    #define mem_v_cap_in_past state[2]
+    #define mem_v_cap_out_past state[3]
+    #define mem_pi_state state[4]
+    #define mem_i_sec_prev state[5]
+
+
+    float v_cap_in, pi_state_next, i_sec, i_pri, v_cap_out;
+
+    float v_dab_in = dab_input(mem_i_in_past, mem_i_pri_prev, mem_v_cap_in_past, &v_cap_in);
+
+
+    /// PI
+
+    float ps = dab_control(in_v_in, mem_v_cap_out_past, in_en, mem_pi_state, &pi_state_next);
+    /// PI
+
+    dab_model(ps, v_dab_in, mem_i_pri_prev, mem_v_cap_out_past, mem_i_sec_prev, &i_sec, &i_pri);
+
+    out_v_out = dab_output(i_sec, in_i_out, mem_v_cap_out_past, in_en, &v_cap_out);
+
+
+
+    out_i_in = dab_input_current(in_v_in, mem_i_in_past, mem_v_cap_in_past, mem_i_pri_prev);
+
+
+    // UPDATE CODE
+
+    mem_pi_state = pi_state_next;
+
+    mem_i_pri_prev = i_pri;
+    mem_i_sec_prev = i_sec;
+
+    mem_v_cap_out_past = v_cap_out;
+    mem_v_cap_in_past = v_cap_in;
+
+    mem_i_in_past = out_i_in;
+
+
+
+    return outputs;
+}
+
+
+
+
+float dab_input(float i_in_past, float i_pri_prev, float v_cap_in_past, float *v_cap_in){
+
+
+    double i_cap_in = i_in_past - i_pri_prev;
+
+    *v_cap_in = v_cap_in_past + t_sw/c_in*i_cap_in;
+
+    float v_dab_in = *v_cap_in + r_esr*i_cap_in;
+
+    return v_dab_in;
+}
+
+
+float dab_control(float setpoint, float fb, float en, float pi_state, float *pi_state_next){
+
+    float err = (setpoint-fb)*en;
+
+    *pi_state_next = pi_state + t_sw*err;
+
+
+    if(*pi_state_next > pi) *pi_state_next = pi;
+    if(*pi_state_next < -pi) *pi_state_next = -pi;
+
+    *pi_state_next = *pi_state_next >  pi ?   pi :  *pi_state_next;
+    *pi_state_next = *pi_state_next < -pi ?  -pi :  *pi_state_next;
+
+    float prop_action = kp*err;
+    float int_action = ki*(*pi_state_next);
+
+    float ps = prop_action + int_action;
+
+    ps = ps >  pi ?   pi :  ps;
+    ps = ps < -pi ?  -pi :  ps;
+
+
+    return ps;
+}
+
+
+
+void dab_model(float ps, float v_dab_in, float i_pri_prev, float v_cap_out_past, float i_sec_prev, float *i_sec, float *i_pri){
+    float ps_rms = ps/sqrt2;
+
+    float ps_factor = ps_rms*(1-2*ps_rms);
+
+
+    float k_dab = n_ps/(f_sw*l_dab);
+
+    *i_sec = (v_dab_in-i_pri_prev*r_dab)*k_dab*ps_factor;
+
+    *i_pri = (v_cap_out_past-i_sec_prev*r_dab)*k_dab*ps_factor;
+
+}
+
+
+float dab_output(float i_sec, float i_out, float v_cap_out_past, float en, float *v_cap_out){
+
+    float i_cap_out = i_sec - i_out;
+
+    *v_cap_out = v_cap_out_past + i_cap_out*t_sw/c_out;
+
+
+    if(en==0){
+        if(*v_cap_out  <0) v_cap_out = 0;
+    }
+
+    float v_out = (*v_cap_out + r_esr*i_cap_out) - r_dc*i_out;
+    return v_out;
+}
+
+
+float dab_input_current(float v_in, float i_in_past, float v_cap_in_past, float i_pri_prev){
+
+    return i_in_past + t_sw/l_dc*(v_in - r_dc*i_in_past - v_cap_in_past -(i_in_past - i_pri_prev)*(t_sw/c_in + r_esr));
+
+}
---
+++ models/cc_dab/cc_dab.hpp
@@ -0,0 +1,33 @@
+#ifndef CC_DAB_HPP
+#define CC_DAB_HPP
+
+#include <vector>
+
+
+extern "C" std::vector<float> cc_dab(const std::vector<float>&inputs, std::vector<float>&state);
+
+
+constexpr float kp = 1E-3;
+constexpr float ki = 1;
+constexpr float r_dab = 3.5e-3;
+constexpr float l_dc = 10e-6;
+constexpr float r_dc = 20e-3;
+constexpr float n_ps = 1;
+constexpr float r_esr = 5e-3;
+constexpr float v_0 = 1000;
+constexpr float f_sw = 25e3;
+constexpr float l_dab = 2e-6;
+constexpr float c_in =  1e-3;
+constexpr float c_out =  1e-3;
+constexpr float pi =  3.14159265358979323846;
+constexpr float sqrt2 = 1.41421356237;
+constexpr  float t_sw = 1/f_sw;
+
+
+float dab_input(float i_in_past, float i_pri_prev, float v_cap_in_past, float *v_cap_in);
+float dab_control(float setpoint, float fb, float en, float pi_state, float *pi_state_next);
+void dab_model(float ps, float v_dab_in, float i_pri_prev, float v_cap_out_past, float i_sec_prev, float *i_sec, float *i_pri);
+float dab_output(float i_sec, float i_out, float v_cap_out_past, float en, float *v_cap_out);
+float dab_input_current(float v_in, float i_in_past, float v_cap_in_past, float i_pri_prev);
+
+#endif //CC_DAB_HPP
